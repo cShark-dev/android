82a3678717c42a65f378b9b7a7e1657e0e2749cd
cShark <asteroidzeta@gmail.com>
2020-05-17 17:21:38 +0100

    170520

--- app/src/main/java/com/example/self/PostJournalActivity.java
+++ app/src/main/java/com/example/self/PostJournalActivity.java
@@ -1,13 +1,19 @@
 package com.example.self;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,13 +23,15 @@ import com.google.firebase.storage.StorageReference;
 
 import util.JournalApi;
 
-public class PostJournalActivity extends AppCompatActivity {
+public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int GALLERY_CODE = 1;
     private Button saveButton;
     private ProgressBar progressBar;
     private ImageView addPhotoButton;
     private EditText titleEditText;
     private EditText thoughtsEditText; //the thing the user will be writing
     private TextView currentUserTextView;
+    private ImageView imageView;
 
     private String currentUserId;
     private String currentUserName;
@@ -37,7 +45,7 @@ public class PostJournalActivity extends AppCompatActivity {
     private StorageReference storageReference;     //to store images
 
     private CollectionReference collectionReference = db.collection("Journal"); //Another reference, a COLLECTION reference, we are passing "Journal",
-
+    private Uri imageUri;
 
 
     @Override
@@ -51,6 +59,73 @@ public class PostJournalActivity extends AppCompatActivity {
         thoughtsEditText = findViewById(R.id.post_description_et);
         currentUserTextView = findViewById(R.id.post_username_textview);
 
+        imageView = findViewById(R.id.post_imageView);      //we now have our imageview, which means we can invoke it to set ouri mage to that.
+        saveButton = findViewById(R.id.post_save_journal_button);
+        saveButton.setOnClickListener(this);
+        addPhotoButton = findViewById(R.id.postCameraButton);
+        addPhotoButton.setOnClickListener(this);
 
+        if (JournalApi.getInstance() != null) {
+            currentUserId = JournalApi.getInstance().getUserId();
+            currentUserName = JournalApi.getInstance().getUsername();
+
+            currentUserTextView.setText(currentUserName);
+        }
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user !=null) {
+
+                } else {
+
+                }
+            }
+        };
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.post_save_journal_button:
+                //saveJournal
+                break;
+            case R.id.postCameraButton:
+                //get image from gallery/phone
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");                                  //we need to give it meta data to tell it what we want to get
+                startActivityForResult(galleryIntent, GALLERY_CODE);                               //we are asking to receive something from the operating system, pass in galleryIntent and gallerycode
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {                   //here we GET the actual image path and we put it inside our image view
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            if (data != null) {                         //make sure data we receive form intent (images in this case) is NOT empty
+                imageUri = data.getData();              //the path to image we are trying to get,
+                imageView.setImageURI(imageUri);//      //show image
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+            super.onStop();
+            if (firebaseAuth != null) {
+                firebaseAuth.removeAuthStateListener((authStateListener));
+            }
     }
 }
